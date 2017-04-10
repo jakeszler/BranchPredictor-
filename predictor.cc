7e602d8e9fd542e7d78de5bf0585d67fa6aa1457
Joseph Zuckerman <jzuckerman@college.harvard.edu>
2017-04-10 02:57:36 +0000

    TAGE written untested

--- predictor.cc
+++ predictor.cc
@@ -3,40 +3,137 @@
 
 PREDICTOR::PREDICTOR (void)
 {
-  
+    numbranches = 0;
+  bp.init_bp();
+  banks.bank_init();
 
 }
 
 
 bool PREDICTOR::GetPrediction (UINT64 PC)
 {
- return true; 
+ numbranches++;
+
+ bool foundPred = false;
+ altpredno = predno = 0;
+ int index = bp.bp_getIndex(PC);
+ pred = altpred = bp.bp_getPred(index);
+
+ for (int i = 0; i < NUMBANKS; i++){
+    int entry = get_bank_index(PC, i, ghr.get_ghr());
+    int tag = get_tag(PC, ghr.get_ghr(), i);
+
+    if (banks.tag_match(i, entry, tag)){
+      if (foundPred){
+        altpred = pred;
+        altpredno = predno;
+      }
+      pred = banks.get_pred(i, entry);
+      predno = i + 1;
+      foundPred = true;
+    }
+ }
+ return pred; 
+
 }
 
 void PREDICTOR::UpdatePredictor (UINT64 PC, OpType OPTYPE, bool resolveDir, bool predDir, UINT64 branchTarget)
 {
+    if (numbranches % 512000 == 0){
+        banks.clearLSBs();
+    }
+    else if (numbranches % 256000 == 0){
+        banks.clearMSBs();
+    }
 
+    int entry = get_bank_index(PC, predno - 1, ghr.get_ghr());
+    if (pred != altpred && predno > 0){
+            //correct, resolve = actual taken or not, pred = predicted taken or not
+            if (resolveDir == predDir)
+                banks.incr_u_ctr(predno - 1, entry);
+            //incorrect
+            else
+                banks.decr_u_ctr(predno - 1, entry);
+    }
 
+    int bp_index = bp.bp_getIndex(PC);
+   
+    //if overall is correct
+    if (resolveDir == predDir){
+        if (predno == 0){
+            bp.bp_update(bp_index, resolveDir);
+        }
+        else{
+            banks.bank_update(predno - 1, entry, resolveDir);
+        }
+    }
+    //if overall is incorrect
+    else{
+        banks.bank_update(predno - 1, entry, resolveDir);
+        if (predno != NUMBANKS){
+            init_update_banks();
+            int prob_val = 1 << (NUMBANKS - 1);
+            int cuml_prob = 0;
+            bool no_zeros = true;
+            for (int i = predno; i < NUMBANKS; i++){
+                 int bank_entry = get_bank_index(PC, i, ghr.get_ghr());
+                 if (banks.get_u_ctr(i, bank_entry) == 0){
+                    update_banks[i] = prob_val;
+                    cuml_prob += prob_val;
+                    prob_val /= 2;
+                    no_zeros = false;
+                 }
+            }
+
+            if (no_zeros){
+                for (int i = predno; i < NUMBANKS; i++){
+                    int bank_entry = get_bank_index(PC, i, ghr.get_ghr());
+                    banks.decr_u_ctr(i, bank_entry);
+                }
+            }
+            else{
+                int rand_val = rand() % cuml_prob;
+                int running_total = 0;
+                int selected_bank = -1;
+
+                for (int i = 0; i < NUMBANKS; i++){
+                    if (update_banks[i] != 0){
+                        running_total += update_banks[i];
+                        if (rand_val < running_total){
+                            selected_bank = i;
+                            break;
+                        }
+                    }
+                }
+
+                //allocate
+                int allocate_entry = get_bank_index(PC, selected_bank, ghr.get_ghr());
+                int allocate_tag = get_tag(PC, ghr.get_ghr(), selected_bank);
+                banks.clear_u_ctr(selected_bank, allocate_entry);
+                banks.set_pred_ctr(selected_bank, allocate_entry);
+                banks.set_tag(selected_bank, allocate_entry, allocate_tag);
+            }
+        }
+    }
 }
 
 void PREDICTOR::TrackOtherInst (UINT64 PC, OpType opType, bool taken, UINT64 branchTarget)
 {
-
 
 }
 
 uint16_t PREDICTOR::get_bank_index(UINT64 PC, uint8_t bankno, __uint128_t ghr){
   int numHistoryBits = (int) (pow(13.0/8.0, bankno) * 10.0 + .5);
   __uint128_t tempGHR = ghr;
-  uint16_t index = tempGHR & ((1 << 10) - 1); 
-  tempGHR >>= 10;
-  numHistoryBits -= 10;
+  uint16_t index = tempGHR & ((1 << BANKINDEXBITS) - 1); 
+  tempGHR >>= BANKINDEXBITS;
+  numHistoryBits -= BANKINDEXBITS;
 
   while (numHistoryBits > 0){
-      if (numHistoryBits >= 10){
-        index ^= tempGHR & ((1 << 10) - 1);
-        tempGHR >>= 10;
-        numHistoryBits -= 10;
+      if (numHistoryBits >= BANKINDEXBITS){
+        index ^= tempGHR & ((1 << BANKINDEXBITS) - 1);
+        tempGHR >>= BANKINDEXBITS;
+        numHistoryBits -= BANKINDEXBITS;
       }
       else{
         index ^= tempGHR & ((1 << numHistoryBits) - 1);
@@ -52,6 +149,11 @@ uint8_t PREDICTOR::get_tag(UINT64 PC, __uint128_t ghr, int bankno){
   return (result % 256);
 }
 
+void PREDICTOR::init_update_banks(){
+    for (int i = 0; i < NUMBANKS; i++){
+        update_banks[i] = 0;
+    }
+}
 
 //Global History Methods
 GHR::GHR(void){
@@ -69,11 +171,11 @@ void GHR::ghr_update(bool resolveDir){
  };
 
 //BASE PREDICTOR METHODS
-
+BasePredictor::BasePredictor(void){}
 
 void BasePredictor::init_bp(){
   for (int i = 0; i < BPSIZE; i++)
-    bp_table[i] = 3;
+    bp_table[i] = 4;
 }
 
 void BasePredictor::bp_incr(int index){
@@ -103,6 +205,8 @@ UINT64 BasePredictor::bp_getIndex(UINT64 PC){
 
 
 //bank methods 
+Banks::Banks(void){};
+
 void Banks::bank_init(){  
   for (int i = 0; i < NUMBANKS; i++){
     for (int j = 0; j < BANKSIZE; j++){
@@ -115,5 +219,68 @@ void Banks::bank_init(){
 
 bool Banks::get_pred(int bank, int entry){
   return (bank_array[bank][entry].pred_ctr & PREDMSB);
+}
+
+bool Banks::tag_match(int bank, int entry, int tag){
+  return (bank_array[bank][entry].tag == tag);
+}
+
+void Banks::set_tag(int bankno, int entry, int tag){
+    bank_array[bankno][entry].tag = tag;
+}
+
+void Banks::incr_pred_ctr(int bank, int entry){
+  if (bank_array[bank][entry].pred_ctr < MAXSAT)
+    bank_array[bank][entry].pred_ctr++;
+}
+  
+void Banks::decr_pred_ctr(int bank, int entry){
+  if (bank_array[bank][entry].pred_ctr > 0)
+    bank_array[bank][entry].pred_ctr--;
+}
+
+void Banks::set_pred_ctr(int bank, int entry){
+    bank_array[bank][entry].pred_ctr = 4;
+}
+
+void Banks::incr_u_ctr(int bank, int entry){
+  if (bank_array[bank][entry].useful_ctr < UMAX)
+    bank_array[bank][entry].useful_ctr++;
+}
+  
+void Banks::decr_u_ctr(int bank, int entry){
+  if (bank_array[bank][entry].useful_ctr > 0)
+    bank_array[bank][entry].useful_ctr--;
+}
+
+void Banks::clear_u_ctr(int bank, int entry){
+    bank_array[bank][entry].useful_ctr = 0;
+}
+
+int Banks::get_u_ctr(int bank, int entry){
+    return bank_array[bank][entry].useful_ctr;
+}
+
+void Banks::bank_update(int bankno, int entry, bool resolveDir){
+  if (resolveDir == TAKEN)
+    incr_pred_ctr(bankno, entry);
+  else
+    decr_pred_ctr(bankno, entry);
+}
+
+void Banks::clearMSBs(){
+    for (int i = 0; i < NUMBANKS; i++){
+        for (int j = 0; j < BANKSIZE; j++){
+            bank_array[i][j].useful_ctr &= 1;
+        }
+    }
+}
+
+void Banks::clearLSBs(){
+    for (int i = 0; i < NUMBANKS; i++){
+        for (int j = 0; j < BANKSIZE; j++){
+            bank_array[i][j].useful_ctr &=  2;
+        }
+    }
 }
 
--- predictor.h
+++ predictor.h
@@ -15,7 +15,9 @@
 
 #define BPSIZE 4096
 #define BANKSIZE 1024
+#define BANKINDEXBITS 10
 #define MAXSAT 7
+#define UMAX 3
 #define PREDMSB 4
 #define BPINDEXBITS 12
 #define TAKEN 1
@@ -38,37 +40,6 @@ uint8_t useful_ctr;
 }bank_entry;
 
 
-
-
-class PREDICTOR{
-
-  // The state is defined for Gshare, change for your design
-
- private:
- // UINT32  ghr;           // global history register
-  //UINT32  *pht;          // pattern history table
-  //UINT32  historyLength; // history length
-  //UINT32  numPhtEntries; // entries in pht 
-
-
- public:
-
-  PREDICTOR(void);
-  //NOTE contestants are NOT allowed to use these versions of the functions
-//ver2   bool    GetPrediction(UINT64 PC, bool btbANSF, bool btbATSF, bool btbDYN);  
-//ver2   void    UpdatePredictor(UINT64 PC, OpType opType, bool resolveDir, bool predDir, UINT64 branchTarget, bool btbANSF, bool btbATSF, bool btbDYN);
-//ver2   void    TrackOtherInst(UINT64 PC, OpType opType, bool branchDir, UINT64 branchTarget);
-
-  // The interface to the functions below CAN NOT be changed
-  bool    GetPrediction(UINT64 PC);  
-  void    UpdatePredictor(UINT64 PC, OpType opType, bool resolveDir, bool predDir, UINT64 branchTarget);
-  void    TrackOtherInst(UINT64 PC, OpType opType, bool branchDir, UINT64 branchTarget);
-
-  uint16_t get_bank_index(UINT64 PC, uint8_t bankno, __uint128_t ghr);
-  uint8_t get_tag(UINT64 PC, __uint128_t ghr);
-  // Contestants can define their own functions below
-};
-
 class BasePredictor{
 private: 
   uint8_t bp_table[BPSIZE];
@@ -89,30 +60,78 @@ UINT64 bp_getIndex(UINT64 PC);
 
 class Banks{
 
-  Banks(void);
-
   private:
     bank_entry bank_array[NUMBANKS][BANKSIZE];
     
   public:
+  Banks(void);
 
   void bank_init();
+  bool tag_match(int bank, int entry, int tag);
   bool get_pred(int bank, int entry);
+  void incr_pred_ctr(int bank, int entry);
+  void decr_pred_ctr(int bank, int entry);
+  void set_pred_ctr(int bank, int entry);
+  void incr_u_ctr(int bank, int entry);
+  void decr_u_ctr(int bank, int entry);
+  void clear_u_ctr(int bank, int entry);
+  void clearMSBs();
+  void clearLSBs();
+  void bank_update(int bankno, int entry, bool resolveDir);
+  void set_tag(int bankno, int entry, int tag);
+  int get_u_ctr(int bank, int entry);
+
 };
 
 class GHR{
 
-  GHR(void);
 
   private:
   __uint128_t ghr;
 
   public:
+  GHR(void);
+
   void ghr_init();
   void ghr_update(bool resolveDir);
   __uint128_t get_ghr();
 
 };
 
+class PREDICTOR{
+
+  // The state is defined for Gshare, change for your design
+
+ private:
+ // UINT32  ghr;           // global history register
+  //UINT32  *pht;          // pattern history table
+  //UINT32  historyLength; // history length
+  //UINT32  numPhtEntries; // entries in pht 
+  GHR ghr;
+  BasePredictor bp;
+  Banks banks;
+  int predno, altpredno;
+  bool pred, altpred;
+  long numbranches; 
+  int update_banks[NUMBANKS];
+
+ public:
+
+  PREDICTOR(void);
+  //NOTE contestants are NOT allowed to use these versions of the functions
+//ver2   bool    GetPrediction(UINT64 PC, bool btbANSF, bool btbATSF, bool btbDYN);  
+//ver2   void    UpdatePredictor(UINT64 PC, OpType opType, bool resolveDir, bool predDir, UINT64 branchTarget, bool btbANSF, bool btbATSF, bool btbDYN);
+//ver2   void    TrackOtherInst(UINT64 PC, OpType opType, bool branchDir, UINT64 branchTarget);
+
+  // The interface to the functions below CAN NOT be changed
+  bool    GetPrediction(UINT64 PC);  
+  void    UpdatePredictor(UINT64 PC, OpType opType, bool resolveDir, bool predDir, UINT64 branchTarget);
+  void    TrackOtherInst(UINT64 PC, OpType opType, bool branchDir, UINT64 branchTarget);
+
+  uint16_t get_bank_index(UINT64 PC, uint8_t bankno, __uint128_t ghr);
+  uint8_t get_tag(UINT64 PC, __uint128_t ghr, int bankno);
+  void init_update_banks();
+  // Contestants can define their own functions below
+};
 #endif
 
